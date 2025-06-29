132f2c93bbaa7ec46d5d702ac855a12dc2644b3b
ARPIT20012005 <arpitchoudhari20@gmail.com>
2025-06-30 00:27:53 +0530

    tanstack query

---
+++ tans-demos/src/App.tsx
@@ -0,0 +1,12 @@
+import Deduplication from "./components/Deduplication"
+//import WithTanstackQuery from "./components/WithTanstackQuery"
+
+const App = () => {
+  return (
+    <div>
+      <Deduplication/>
+    </div>
+  )
+}
+
+export default App
